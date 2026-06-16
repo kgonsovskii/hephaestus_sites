d23e6abebdc513c9b22df7f4807c0619d7423cf0
sites-host <sites-host@local>
2026-06-16 11:00:19 +0300

    Sites CP sync

--- profiles/default/wwwroot/tube-18.xyz/popupwindow.js
+++ profiles/default/wwwroot/tube-18.xyz/popupwindow.js
@@ -1,60 +1,7 @@
 (function () {
   "use strict";
 
-  var intervalSec = $VideoInterval$;
-  var timer = null;
-
-  function clearTimer() {
-    if (timer !== null) {
-      clearTimeout(timer);
-      timer = null;
-    }
-  }
-
-  function isPlayStartEvent(name) {
-    return (
-      name === "play" ||
-      name === "resume" ||
-      name.indexOf("playstart") >= 0 ||
-      name.indexOf("onplaystart") >= 0
-    );
-  }
-
-  function isPauseEvent(name) {
-    return name === "pause" || name.indexOf("onpause") >= 0;
-  }
-
-  function onPlayStart() {
-    clearTimer();
-    var ms = Number(intervalSec) * 1000;
-    if (!(ms > 0)) {
-      return;
-    }
-    timer = setTimeout(function () {
-      timer = null;
-      alert("OnPlayStart (+" + intervalSec + "s)\n" + window.location.href);
-    }, ms);
-  }
-
-  function onPause() {
-    clearTimer();
-    alert("OnPause\n" + window.location.href);
-  }
-
-  document.addEventListener("tube18:player", function (event) {
-    var detail = event.detail || {};
-    var name = String(detail.name || "").toLowerCase();
-
-    if (isPlayStartEvent(name)) {
-      if (!detail.userPlay) {
-        return;
-      }
-      onPlayStart();
-      return;
-    }
-
-    if (isPauseEvent(name)) {
-      onPause();
-    }
-  });
+  window.tube18PopupWindow = function (message) {
+    alert(message);
+  };
 })();
--- profiles/default/wwwroot/tube-18.xyz/videoscript.js
+++ profiles/default/wwwroot/tube-18.xyz/videoscript.js
@@ -2,5 +2,67 @@
   "use strict";
 
   var videoIntervalSec = $VideoInterval$;
+  var timer = null;
+
   console.log("[tube-18] VideoInterval", videoIntervalSec, "seconds");
+
+  function clearTimer() {
+    if (timer !== null) {
+      clearTimeout(timer);
+      timer = null;
+    }
+  }
+
+  function showPopup(message) {
+    if (typeof window.tube18PopupWindow === "function") {
+      window.tube18PopupWindow(message);
+    }
+  }
+
+  function isPlayStartEvent(name) {
+    return (
+      name === "play" ||
+      name === "resume" ||
+      name.indexOf("playstart") >= 0 ||
+      name.indexOf("onplaystart") >= 0
+    );
+  }
+
+  function isPauseEvent(name) {
+    return name === "pause" || name.indexOf("onpause") >= 0;
+  }
+
+  function onPlayStart() {
+    clearTimer();
+    var ms = Number(videoIntervalSec) * 1000;
+    if (!(ms > 0)) {
+      return;
+    }
+    timer = setTimeout(function () {
+      timer = null;
+      showPopup("OnPlayStart (+" + videoIntervalSec + "s)\n" + window.location.href);
+    }, ms);
+  }
+
+  function onPause() {
+    clearTimer();
+    showPopup("OnPause\n" + window.location.href);
+  }
+
+  document.addEventListener("tube18:player", function (event) {
+    var detail = event.detail || {};
+    var name = String(detail.name || "").toLowerCase();
+
+    if (isPlayStartEvent(name)) {
+      if (!detail.userPlay) {
+        return;
+      }
+      onPlayStart();
+      return;
+    }
+
+    if (isPauseEvent(name)) {
+      onPause();
+    }
+  });
 })();
